057aec5cfae416351a47ade60191f9c6869d61a3
gary909 <garywhite61@hotmail.com>
2022-08-31 13:41:15 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function factorial(n) {
+    // Calculate the factorial here
+}
+
+console.log(factorial(0)); // "factorial for 0 is 1"
+console.log(factorial(1)); // "factorial for 1 is 1"
+console.log(factorial(2)); // "factorial for 2 is 2"
+console.log(factorial(3)); // "factorial for 3 is 6"
+console.log(factorial(5)); // "factorial for 5 is 120"
